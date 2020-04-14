1b2dbe6a3d23650e6ee8294fbe28d31fba6bc22f
le.david993@gmail.com <le.david993@gmail.com>
2020-04-13 21:40:22 -0500

    menu item

---
+++ src/components/MenuItem/MenuItem.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Box } from '@chakra-ui/core'
+import {} from 'react-icons'
+
+const MenuItem: React.FC = () => {
+  return <Box>hello</Box>
+}
+
+export default MenuItem
